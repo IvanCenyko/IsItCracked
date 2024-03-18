cf1fd7855dbb5a96006ac3166185d72c44e7aa80
IvanCenyko <ivancenyko@gmail.com>
2024-03-17 22:03:27 -0300

    add example request script, rename django app

--- web_api/test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-
-r = requests.get("http://127.0.0.1:8000/search", {"search": "five nights at freddys"})
-#r = requests.get("https://is-it-cracked-web.vercel.app/game-status", {"url": "https://omycrack.com/g/557970"})
-print(r.json())
-
-
-
